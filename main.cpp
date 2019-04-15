e605912c80afc69503b77f1bbca7993051f4831f
Cameron Hosking <hippiemancam@gmail.com>
2019-04-15 12:33:18 +1000

    Resets Kmer whenever encountering an Alpha character that is not A,C,G, or T break output and newIDline out into separate functions.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <chrono>
 
+
 #include "readArgs.h"
 
 std::string get_file_contents(const std::string &filename, bool verbose = false)
@@ -31,7 +32,7 @@ std::string get_file_contents(const std::string &filename, bool verbose = false)
 constexpr char chars[4] = { 'A','C','G','T' };
 
 template <unsigned int K,bool OutputZeros>
-void output(int index, std::string &kmer, const std::unique_ptr<int[]> &results, double denominator, std::ostream &os)
+void output(int index, std::string &kmer, const uint32_t results[], double denominator, std::ostream &os)
 {
 	for (int i = 0; i < 4; ++i)
 	{
@@ -42,95 +43,24 @@ void output(int index, std::string &kmer, const std::unique_ptr<int[]> &results,
 
 //base case prints out the kmer and associated frequency
 template<>
-void output<0,true>(int index, std::string &kmer, const std::unique_ptr<int[]> &results, double denominator, std::ostream &os)
+void output<0,true>(int index, std::string &kmer, const uint32_t results[], double denominator, std::ostream &os)
 {
 	os << kmer << '\t' << results[index] *denominator << '\n';
 }
 
 template<>
-void output<0, false>(int index, std::string &kmer, const std::unique_ptr<int[]> &results, double denominator, std::ostream &os)
+void output<0, false>(int index, std::string &kmer, const uint32_t results[], double denominator, std::ostream &os)
 {
 	int count = results[index];
 	if (count != 0)
 		os << kmer << '\t' << count*denominator << '\n';
 }
 
-template <unsigned int K>
-void countKmers(const std::string &s, bool outputZeros, bool split)
+//outputs the kmers frequency, if split is true it outputs the id as the first line,
+//if output zeros is true then output kmers with zero counts
+template<unsigned int K>
+void output(size_t i, std::string id, bool split, bool outputZeros, const uint32_t results[], size_t total)
 {
-	auto start = std::chrono::high_resolution_clock::now();
-	//allocate memory for the 4^K different kmers
-	uint64_t kmers = 1LL << K * 2;
-	std::unique_ptr<int[]> results = std::unique_ptr<int[]>(new int[kmers]());
-	std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << std::endl;
-	start = std::chrono::high_resolution_clock::now();
-	//creates a bitmask that we can slap on a coded string to grab the last K characters
-	uint64_t mask = kmers - 1;
-	size_t count = 0;
-	uint64_t currentString = 0;//encodes the current string;
-	size_t total = 0;
-	std::string id;
-	size_t i = 0;
-	for (; i < s.length(); i++)
-	{
-		char c = s[i];
-		//all alpha characters
-		if (c & 64)
-		{
-			if (c == 'N' || c == 'R')//unknown characters
-			{
-				count = 0;
-			}
-			else
-			{//this record the character ACGT or acgt with a value of 0x00 for Aa 0x01 for Cc 0x11 for Gg and 0x10 for Tt
-				count++;
-				//this v represents the character ACGT or acgt with a value of 0x00 for Aa, 0x01 for Cc, 0x11 for Gg, and 0x10 for Tt
-				uint8_t v = (c & 7) >> 1;
-				//xoring with itself bitshefted right by 1 doesnt effect A or C but flips the small bit of G and T
-				v ^= v >> 1;
-				//the letters are now alphebetically sorted with A=0, C=1, G=2, T=3
-				currentString += v;
-				if (count >= K)
-				{
-					results[currentString&mask]++;
-					total += 1;
-				}
-				currentString <<= 2;
-			}
-		}
-		else if(c== '>')
-		{
-			//if split is true then we ouput individual counts for each id found
-			if (split && total > 0)
-			{
-				std::string outFileName = "output" + std::to_string(i) + ".txt";
-				std::ofstream out(outFileName, std::ostream::trunc | std::ostream::binary);
-				std::cout << "outputting Kmers to: " << outFileName << std::endl;
-				out << id << std::endl;
-				if (outputZeros)
-				{
-					output<K, true>(0, std::string(K, ' '), results, 1.0 / (double)total, out);
-				}
-				else
-				{
-					output<K, false>(0, std::string(K, ' '), results, 1.0 / (double)total, out);
-				}
-				out.close();
-				total = 0;
-				//zero the results
-				memset(results.get(), 0, kmers*sizeof(int));
-			}
-			size_t endOfLine = s.find_first_of('\n', i);
-			id = s.substr(i, endOfLine - i);
-			std::cout << "counting Kmers in:" << id << std::endl;
-			i = endOfLine;
-			count = 0;
-		}
-		//other characters are ignored eg \n\r etc
-	}
-	std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << std::endl;
-	start = std::chrono::high_resolution_clock::now();
-	
 	//output string
 	std::string outFileName = "output" + std::to_string(i) + ".txt";
 	std::ofstream out(outFileName, std::ostream::trunc | std::ostream::binary);
@@ -147,6 +77,96 @@ void countKmers(const std::string &s, bool outputZeros, bool split)
 		output<K, false>(0, std::string(K, ' '), results, 1.0 / (double)total, out);
 	}
 	out.close();
+}
+
+//call at the start of an id line, splits and outputs is split is true
+//set the id to the contents of this line
+//return the index at the end of the line
+template<unsigned int K>
+size_t newID(bool split, size_t total, size_t i, std::string &id, bool outputZeros, uint32_t results[], const std::string &s)
+{
+	uint64_t kmers = 1LL << K * 2;
+	//if split is true then we ouput individual counts for each id found
+	if (split && total > 0)
+	{
+		output<K>(i, id, split, outputZeros, results, total);
+		//zero the results
+		memset(results, 0, kmers*sizeof(int));
+	}
+	size_t endOfLine = s.find_first_of('\n', i);
+	id = s.substr(i, endOfLine - i);
+	std::cout << "counting Kmers in:" << id << std::endl;
+	return endOfLine;
+}
+
+//converts Aa=0, Cc=1, Gg=2, Tt=3, some other value between 0 and 3 for all other characters
+inline uint8_t charTo2bit(char c)
+{
+	//this v represents the character ACGT or acgt with a value of 0x00 for Aa, 0x01 for Cc, 0x11 for Gg, and 0x10 for Tt
+	uint8_t v = (c & 7) >> 1;
+	//xoring with itself bitshifted right by 1 doesnt effect A or C but flips the small bit of G and T
+	return v ^ (v >> 1);
+}
+
+template <unsigned int K>
+void countKmers(const std::string &s, bool outputZeros, bool split)
+{
+	auto start = std::chrono::high_resolution_clock::now();
+	//allocate memory for the 4^K different kmers
+	uint64_t kmers = 1LL << K * 2;
+	std::unique_ptr<uint32_t[]> results = std::unique_ptr<uint32_t[]>(new uint32_t[kmers]());
+	std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << std::endl;
+	start = std::chrono::high_resolution_clock::now();
+	//creates a bitmask that we can slap on a coded string to grab the last K characters
+	uint64_t mask = kmers - 1;
+	size_t count = 0;
+	uint64_t currentString = 0;//encodes the current string;
+	size_t total = 0;
+	std::string id;
+	size_t i = 0;
+
+	for (; i < s.length(); i++)
+	{
+		char c = s[i];
+
+		//all alpha characters
+		if (c & 64)
+		{
+			//if the character is any of upper or lowercase ACGT
+			char cLast5bits = c & 31;
+			if (cLast5bits == ('A' & 31) || cLast5bits == ('C' & 31) || cLast5bits == ('G' & 31) || cLast5bits == ('T' & 31))
+			{
+				count++;
+				//convert the character to a 2bit number and add it to the end of the current string
+				currentString += charTo2bit(c);
+				if (count >= K)
+				{
+					//increment the number of counts for this particular string
+					results[currentString&mask]++;
+					total += 1;
+				}
+				currentString <<= 2;
+			}//otherwise it is an unknown so reset the count
+			else
+			{
+				count = 0;
+			}
+		}
+		else if(c== '>')
+		{
+			i = newID<K>(split, total, i, id, outputZeros, results.get(), s);
+			if (split)
+			{
+				total = 0;
+			}
+			count = 0;
+		}
+		//other characters are ignored eg \n\r etc
+	}
+	std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << std::endl;
+	start = std::chrono::high_resolution_clock::now();
+	
+	output<K>(i, id, split, outputZeros, results.get(), total);
 
 	std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << std::endl;
 }
