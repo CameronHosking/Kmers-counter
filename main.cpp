54ce731bc6b7b718f532ee283b80aa4311b674a4
Cameron Hosking <cameron.j.hosking@gmail.com>
2019-04-17 18:02:06 +1000

    output kmers for multiple K at once experimental

--- main.cpp
+++ main.cpp
@@ -57,6 +57,35 @@ public:
 	}
 };
 
+class OutPutter
+{
+	std::ostream *out;
+	bool readingFile;
+public:
+	OutPutter()
+		:out(&std::cout), readingFile(false)
+	{}
+
+	void open(const std::string &filename)
+	{
+		if (readingFile)
+			delete out;
+		out = new std::ofstream(filename, std::ostream::trunc | std::ostream::binary);
+		readingFile = true;
+	}
+
+	std::ostream &getStream()
+	{
+		return *out;
+	}
+
+	~OutPutter()
+	{
+		if (readingFile)
+			delete out;
+	}
+};
+
 struct Parameters
 {
 	std::string inputFile;
@@ -65,10 +94,13 @@ struct Parameters
 	bool outputZeros;
 	bool outputToFile;
 	unsigned int k;
+	bool allKs;
+
 	Parameters(int argc, char** argv)
 	{
 		outputZeros = cmdLineArgumentFound(argc, argv, "outputZeros")|cmdLineArgumentFound(argc, argv, "z");
 		split = cmdLineArgumentFound(argc, argv, "split")|cmdLineArgumentFound(argc, argv, "s");
+		allKs = cmdLineArgumentFound(argc, argv, "allUpToK") | cmdLineArgumentFound(argc, argv, "a");
 
 		if (!(getCmdLineArgument(argc, argv, "k", k) || getCmdLineArgument(argc, argv, "K", k)) || k<1 || k>15)
 		{
@@ -118,14 +150,9 @@ void output<0, false>(int index, std::string &kmer, const uint32_t results[], do
 		os << kmer << '\t' << count*denominator << '\n';
 }
 
-//outputs the kmers frequency, if split is true it outputs the id as the first line,
-//if output zeros is true then output kmers with zero counts
-template<unsigned int K>
-void output(Parameters &p, std::string id, const uint32_t results[], size_t total, int idsFound)
+//prepares an outputter
+void prepareOutputStream(const Parameters &p, std::string id, int idsFound, OutPutter &out)
 {
-	//output string
-	std::string baseKmer = std::string(K, ' ');
-	std::ostream *out = &std::cout;
 	if (p.outputToFile)
 	{
 		std::string outFileName = p.outputFile;
@@ -141,40 +168,62 @@ void output(Parameters &p, std::string id, const uint32_t results[], size_t tota
 				outFileName = outFileName.substr(0, pos) + std::to_string(idsFound) + outFileName.substr(pos + 1);
 			}
 		}
-		out = new std::ofstream(outFileName, std::ostream::trunc| std::ostream::binary);
+		out.open(outFileName);
 		std::cout << "outputting Kmers to: " << outFileName << std::endl;
 	}
-	if (p.split) {
-		*out << id << std::endl;
-	}
-	if (p.outputZeros)
+	if (p.split)
 	{
-		output<K, true>(0, baseKmer, results, 1.0 / (double)total, *out);
-	}
-	else
-	{
-		output<K, false>(0, baseKmer, results, 1.0 / (double)total, *out);
-	}
-	if (p.outputFile != std::string())
-	{
-		delete out;
+		out.getStream() << id << std::endl;
 	}
 }
 
-//call at the start of an id line, splits and outputs is split is true
+//outputs the kmers frequency, if split is true it outputs the id as the first line,
+//if output zeros is true then output kmers with zero counts
+template<unsigned int K>
+void output(const Parameters &p, const uint32_t results[], size_t total, std::ostream &out)
+{
+	//output string
+	std::string baseKmer = std::string(K, ' ');
+	if (p.outputZeros)
+	{
+		output<K, true>(0, baseKmer, results, 1.0 / (double)total, out);
+	}
+	else
+	{
+		output<K, false>(0, baseKmer, results, 1.0 / (double)total, out);
+	}
+}
+
+//template<unsigned int K, unsigned int S>
+//class outputMultiple
+//
+template<unsigned int K>
+class outputMultiple
+{
+public:
+
+	template<unsigned int S=1>
+	static void doit(const Parameters &p, const std::unique_ptr<uint32_t[]> results[K], size_t totals[K], std::ostream &out)
+	{
+		output<S>(p, results[S-1].get(), totals[S-1] + totals[K - 1], out);
+		doit<S+1>(p, results, totals, out);
+	}
+
+	template<>
+	static void doit<K>(const Parameters &p, const std::unique_ptr<uint32_t[]> results[K], size_t totals[K], std::ostream &out)
+	{
+		output<K>(p, results[K - 1].get(), totals[K - 1], out);
+	}
+
+
+};
+
+//call at the start of an id line
 //set the id to the contents of this line
 //return the index at the end of the line
-template<unsigned int K>
-size_t newID(Parameters &p, size_t total, size_t i, std::string &id, uint32_t results[], std::string &s, InputReader &in, int idsFound)
+size_t newID(const Parameters &p, size_t i, std::string &id, std::string &s, InputReader &in)
 {
-	uint64_t kmers = 1LL << K * 2;
-	//if split is true then we ouput individual counts for each id found
-	if (p.split && total > 0)
-	{
-		output<K>(p, id, results, total, idsFound);
-		//zero the results
-		memset(results, 0, kmers*sizeof(int));
-	}
+
 	size_t endOfLine;
 	id = "";
 	//this is a while loop in case somehow the id length is longer than the read buffer we're using
@@ -201,18 +250,39 @@ inline uint8_t charTo2bit(char c)
 	return v ^ (v >> 1);
 }
 
-template <unsigned int K>
-void countKmers(InputReader &input, Parameters &p)
+template <unsigned int K, bool allUpToK>
+void countKmers(InputReader &input,const Parameters &p)
 {
 	auto start = std::chrono::high_resolution_clock::now();
 	//allocate memory for the 4^K different kmers
 	uint64_t kmers = 1LL << K * 2;
-	std::unique_ptr<uint32_t[]> results = std::unique_ptr<uint32_t[]>(new uint32_t[kmers]());
+	std::unique_ptr<uint32_t[]> results;
+	std::unique_ptr<uint32_t[]> resultsForall[K];
 	//creates a bitmask that we can slap on a coded string to grab the last K characters
-	uint64_t mask = kmers - 1;
-	size_t count = 0;
+	uint64_t mask;
+	uint64_t masks[K];
+	size_t total;
+	size_t totals[K];
+
+	if (allUpToK)
+	{
+		for (int i = 0; i < K; ++i)
+		{
+			uint64_t kmers = 1LL << (i+1) * 2;
+			masks[i] = kmers - 1;
+			totals[i] = 0;
+			resultsForall[i] = std::unique_ptr<uint32_t[]>(new uint32_t[kmers]());
+		}
+	}
+	else
+	{
+		mask = kmers - 1;
+		total = 0;
+		results = std::unique_ptr<uint32_t[]>(new uint32_t[kmers]());
+	}
+
 	uint64_t currentString = 0;//encodes the current string;
-	size_t total = 0;
+	size_t count = 0;
 	std::string id;
 	int idsFound = 0;
 	std::string s;
@@ -230,14 +300,41 @@ void countKmers(InputReader &input, Parameters &p)
 				char cLast5bits = c & 31;
 				if (cLast5bits == ('A' & 31) || cLast5bits == ('C' & 31) || cLast5bits == ('G' & 31) || cLast5bits == ('T' & 31))
 				{
-					count++;
 					//convert the character to a 2bit number and add it to the end of the current string
 					currentString += charTo2bit(c);
-					if (count >= K)
+					count++;
+
+					if(allUpToK)
 					{
-						//increment the number of counts for this particular string
-						results[currentString&mask]++;
-						total += 1;
+						if (count >= K)
+						{
+
+							for (int Kindex = 0; Kindex < K; ++Kindex)
+							{
+								resultsForall[Kindex][currentString&masks[Kindex]]++;
+							}
+							//to avoid unneccessary work we only add to the last totals
+							//to get the totals for the Kmers less than length K we just add
+							//this one to their total
+							totals[K - 1]++;
+						}
+						else
+						{
+							for (int Kindex = 0; Kindex < count; ++Kindex)
+							{
+								resultsForall[Kindex][currentString&masks[Kindex]]++;
+								totals[Kindex]++;
+							}
+						}						
+					}
+					else
+					{
+						if (count >= K)
+						{
+							//increment the number of counts for this particular string
+							results[currentString&mask]++;
+							total += 1;
+						}
 					}
 					currentString <<= 2;
 				}//otherwise it is an unknown so reset the count
@@ -248,12 +345,32 @@ void countKmers(InputReader &input, Parameters &p)
 			}
 			else if (c == '>')
 			{
-				i = newID<K>(p, total, i, id, results.get(), s, input, idsFound);
-				idsFound++;
-				if (p.split)
+				//if split is true then we ouput individual counts for each id found
+				if (p.split&&(allUpToK?totals[0]:total) > 0)
 				{
-					total = 0;
-				}
+					OutPutter out;
+					prepareOutputStream(p, id, idsFound, out);
+					if (allUpToK)
+					{
+						outputMultiple<K>::doit(p, resultsForall, totals, out.getStream());
+						for (int Kindex = 0; Kindex < K; ++Kindex)
+						{
+							totals[Kindex] = 0;
+							int kmers = (1LL << (Kindex + 1) * 2);
+							memset(resultsForall[Kindex].get(), 0, kmers * sizeof(int));
+						}
+					}
+					else
+					{
+						output<K>(p, results.get(), total, out.getStream());
+						//zero the results
+						memset(results.get(), 0, kmers * sizeof(int));
+						total = 0;
+					}
+				}	
+
+				i = newID(p,  i, id, s, input);
+				idsFound++;
 				count = 0;
 			}
 			//other characters are ignored eg \n\r etc
@@ -262,10 +379,33 @@ void countKmers(InputReader &input, Parameters &p)
 	if (p.outputToFile)
 		std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << std::endl;
 	start = std::chrono::high_resolution_clock::now();
+
+	OutPutter out;
+	prepareOutputStream(p, id, idsFound, out);
+	if (allUpToK)
+	{
+		outputMultiple<K>::doit(p, resultsForall, totals, out.getStream());
+	}
+	else{
+		output<K>(p, results.get(), total, out.getStream());
+	}
 	
-	output<K>(p, id, results.get(), total, idsFound);
 	if (p.outputToFile)
 		std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << std::endl;
+}
+
+//this wraps countKmers so that we can compile in the boolean of whether or not to count all up to kmers
+template <unsigned int K>
+void countMultiKmers(InputReader &input, const Parameters &p)
+{
+	if (p.allKs)
+	{
+		countKmers<K, true>(input, p);
+	}
+	else
+	{
+		countKmers<K, false>(input, p);
+	}
 }
 
 int main(int argc, char** argv)
@@ -281,21 +421,21 @@ int main(int argc, char** argv)
 	//this allows us to select a K at runtime even though it is a compile time constant
 	switch (p.k)
 	{
-	case 1: countKmers<1>(s, p); break;
-	case 2: countKmers<2>(s, p); break;
-	case 3: countKmers<3>(s, p); break;
-	case 4: countKmers<4>(s, p); break;
-	case 5: countKmers<5>(s, p); break;
-	case 6: countKmers<6>(s, p); break;
-	case 7: countKmers<7>(s, p); break;
-	case 8: countKmers<8>(s, p); break;
-	case 9: countKmers<9>(s, p); break;
-	case 10: countKmers<10>(s, p); break;
-	case 11: countKmers<11>(s, p); break;
-	case 12: countKmers<12>(s, p); break;
-	case 13: countKmers<13>(s, p); break;
-	case 14: countKmers<14>(s, p); break;
-	case 15: countKmers<15>(s, p); break;
+	case 1: countMultiKmers<1>(s, p); break;
+	case 2: countMultiKmers<2>(s, p); break;
+	case 3: countMultiKmers<3>(s, p); break;
+	case 4: countMultiKmers<4>(s, p); break;
+	case 5: countMultiKmers<5>(s, p); break;
+	case 6: countMultiKmers<6>(s, p); break;
+	case 7: countMultiKmers<7>(s, p); break;
+	case 8: countMultiKmers<8>(s, p); break;
+	case 9: countMultiKmers<9>(s, p); break;
+	case 10: countMultiKmers<10>(s, p); break;
+	case 11: countMultiKmers<11>(s, p); break;
+	case 12: countMultiKmers<12>(s, p); break;
+	case 13: countMultiKmers<13>(s, p); break;
+	case 14: countMultiKmers<14>(s, p); break;
+	case 15: countMultiKmers<15>(s, p); break;
 	}
 	return 0;
 }
